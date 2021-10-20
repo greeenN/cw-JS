83fc76976e1be14ac1e0de56868081b69affc5ee
greeenN <nikgrishin1999@mail.ru>
2021-10-20 12:38:25 +0300

    added 12

--- script.js
+++ script.js
@@ -79,3 +79,14 @@
 /*function abbrevName(name){
     return name.match(/[A-Z]/g).join('.');
 }*/
+
+// 12
+
+/*
+function isDivisible(n, x, y) {
+    if (n % x == 0 && n % y == 0) {
+        return true;
+    } else {
+        return false;
+    }
+}*/
