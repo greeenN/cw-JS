ce6dc6fe9f2dac49d801ee94d6ce402fe807a2aa
greeenN <nikgrishin1999@mail.ru>
2021-10-21 00:47:30 +0300

    added 18

--- script.js
+++ script.js
@@ -136,3 +136,13 @@ function greet(name){
         return result;
     }
 }*/
+
+// 18
+
+/*function isDivideBy(number, a, b) {
+    if (number % a == 0 && number % b == 0) {
+        return true;
+    } else {
+        return false;
+    }
+}*/
