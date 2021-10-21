53784f64be3d1ee3fea9a95bd0eb060d444e6269
greeenN <nikgrishin1999@mail.ru>
2021-10-22 00:41:17 +0300

    "added 37"

--- script.js
+++ script.js
@@ -304,3 +304,13 @@ var totalGoals = laLigaGoals + championsLeagueGoals + copaDelReyGoals;*/
         return false;
     }
 }*/
+
+// 37
+
+/*function squareSum(numbers){
+    var sum = 0;
+    for (var i = 0; i < numbers.length; i++) {
+        sum += Math.pow(numbers[i], 2);
+    }
+    return sum;
+}*/
