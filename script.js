f4c7766942ae0526815ed0a38ef71a016b42feaa
greeenN <nikgrishin1999@mail.ru>
2021-10-21 13:12:51 +0300

    added 19

--- script.js
+++ script.js
@@ -146,3 +146,14 @@ function greet(name){
         return false;
     }
 }*/
+
+// 19
+
+/*
+function hoopCount (n) {
+    if (n >= 10) {
+        return "Great, now move on to tricks";
+    } else {
+        return "Keep at it until you get it";
+    }
+}*/
